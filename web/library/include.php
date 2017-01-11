e05e6d1112514a863efab528eff584c32d96a8bf
Lee Crosby <ligand@hotmail.co.uk>
2017-01-11 23:49:50 +0000

    Delete include.php

--- web/library/include.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require_once(realpath(dirname(__FILE__) . "/../config/config.php"));
-	
-	require_once(realpath(dirname(__FILE__) . "/MoteClient.php"));
-	require_once(realpath(dirname(__FILE__) . "/ErrorResponse.php"));
-?>
